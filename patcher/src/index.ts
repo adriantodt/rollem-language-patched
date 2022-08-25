1a64b438a32fba4d51cd2fcc8f8f5416049d2b9a
AdrianTodt <adriantodt.ms@gmail.com>
2022-08-25 11:01:35 -0300

    Minor optimizations

--- patcher/src/index.ts
+++ patcher/src/index.ts
@@ -1,6 +1,6 @@
 import { createLogger } from './utils/logger';
 import { ExitCode } from './utils/exitCode';
-import { copyFile, mkdir, readFile, writeFile } from 'fs/promises';
+import { copyFile, mkdir, readFile, stat, writeFile } from 'fs/promises';
 import fetch from 'node-fetch';
 import tar from 'tar';
 import klaw from 'klaw';
@@ -22,17 +22,20 @@ async function getVersion() {
 }
 
 async function downloadAndExtract(version: string) {
-  let dir = `./.downloaded/${version}`;
+  const dir = `./.downloaded/${version}`;
   await mkdir(dir, { recursive: true });
   await fetch(`https://registry.npmjs.org/@rollem/language/-/language-${version}.tgz`)
     .then(r => r.body!.pipe(tar.x({ cwd: dir })));
 }
 
+const whitelistedDependencies = ['lodash', 'chance'];
+
 function patchPackageJson(json: any): any {
-  const whitelistedDependencies = ['lodash', 'chance'];
   delete json.scripts;
   delete json.mocha;
   delete json.devDependencies;
+  delete json.gitHead;
+  delete json.publishConfig;
   json.name = '@adriantodt/rollem-language';
   json.dependencies = Object.fromEntries(
     Object.entries(json.dependencies).filter(([key]) => whitelistedDependencies.includes(key)),
@@ -46,12 +49,29 @@ function patchPackageJson(json: any): any {
   json.bugs = {
     url: 'https://github.com/adriantodt/rollem-language-patched/issues',
   }
-  return json;
+  return sortPackageJson(json);
+}
+
+const packageJsonKeyOrder = [
+  'name', 'version', 'description', 'keywords', 'license', 'author', 'contributors',
+  'homepage', 'bugs', 'main', 'module', 'umd:main', 'types', 'files',
+  'dependencies', 'devDependencies', 'peerDependencies', 'scripts',
+];
+
+function sortPackageJson(json: any) {
+  return Object.fromEntries(
+    Object.entries(json).sort(([k1], [k2]) => {
+      const i1 = packageJsonKeyOrder.indexOf(k1), i2 = packageJsonKeyOrder.indexOf(k2);
+      return i1 === -1 ? 1 : i2 === -1 ? -1 : i1 - i2;
+    }),
+  );
 }
 
 async function nukeDist() {
   await promisify(rimraf)('dist');
 }
+
+const filesToIgnoreRegExp = /(?:\.spec\.(?:d\.ts|js)|\.gitignore|\.npmignore|README\.md)$/;
 
 async function copyDownloadedFiles(version: string) {
   for await (const file of klaw(`./.downloaded/${version}/package`)) {
@@ -74,17 +94,30 @@ async function copyDownloadedFiles(version: string) {
             2,
           ),
         );
-      } else if (!/(?:\.spec\.(?:d\.ts|js)|.gitignore|.npmignore|README.md)$/.test(relativePath)) {
-        await copyFile(file.path, `../${relativePath}`);
+      } else if (!filesToIgnoreRegExp.test(relativePath)) {
+        if (relativePath == 'LICENSE') {
+          await writeFile(`../${relativePath}`, await readFile(file.path, 'utf8'));
+        } else {
+          await copyFile(file.path, `../${relativePath}`);
+        }
       }
     }
   }
 }
 
+async function downloadIfNotExists(version: string) {
+  const stats = await stat(`./.downloaded/${version}`).catch(() => null);
+  if (!stats || !stats.isDirectory()) {
+    logger.info(`Downloading @rollem/language v${version}...`);
+    await downloadAndExtract(version);
+  } else {
+    logger.info(`Using existing @rollem/language v${version}...`);
+  }
+}
+
 async function main() {
   const version = await getVersion();
-  logger.info(`Downloading @rollem/language v${version}...`);
-  await downloadAndExtract(version);
+  await downloadIfNotExists(version);
   logger.info(`Patching repository with downloaded files...`);
   await nukeDist();
   await copyDownloadedFiles(version);
